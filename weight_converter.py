b6381f2f80d7e04d14d8bdd616bee7d3ce5514b9
guyitti2000 <polyglotpengyou@gmail.com>
2021-06-13 17:15:12 +0700

    added a weight converter program

--- weight_converter.py
+++ weight_converter.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python
 
-print("weight")
+
+amount = int(input("Amount of weight: "))
+unit = input("(K)g or (L)bs: ")
+
+if unit.upper() == "K":
+    converted = amount * 2.20462
+    print(str(converted) + " lbs")
+else:
+    converted = amount * 0.454
+    print(str(converted) + " kg")
+
+
+
