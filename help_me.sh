d30b7e4bf43af8495a0ad4ebdb6f7f9bda3fc709
guyitti2000 <polyglotpengyou@gmail.com>
2021-06-16 12:14:59 +0700

    added old stuff

---
+++ help_me.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+
+
+
+
+python rob.py && cd ../../Music/background_music/ && play *mp3 
---
+++ rob.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+
+
+
+
+x = "3"
+y = ".14"
+
+print("Rob is equivalent to " + x + y)
