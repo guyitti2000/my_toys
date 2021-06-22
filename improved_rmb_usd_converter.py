a7f06817dfc5e353f42e9a1acfe839ae1b2a0a53
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-06-22 07:49:50 +0700

    added what it is supposded to look like

--- improved_rmb_usd_converter.py
+++ improved_rmb_usd_converter.py
@@ -18,14 +18,42 @@ print('The conversion are:')
 for amount in rmb:
     print(' ' + str(rmb))
 
-
+#supposed to look something like that below
 '''
-print("We have converted {}rmb".format(amount) +  " to " + str(convertandCompare(amount)))
-'''
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+      ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]  # list concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)
 
+When you run this program, the output will look something like this:
 
-'''
-print(str(convertandCompare(rmb1)))
-print(convertandCompare(rmb2))
-print(convertandCompare(rmb3))
+Enter the name of cat 1 (Or enter nothing to stop.):
+Zophie
+Enter the name of cat 2 (Or enter nothing to stop.):
+Pooka
+Enter the name of cat 3 (Or enter nothing to stop.):
+Simon
+Enter the name of cat 4 (Or enter nothing to stop.):
+Lady Macbeth
+Enter the name of cat 5 (Or enter nothing to stop.):
+Fat-tail
+Enter the name of cat 6 (Or enter nothing to stop.):
+Miss Cleo
+Enter the nameof cat 7 (Or enter nothing to stop.):
+
+The cat names are:
+  Zophie
+  Pooka
+  Simon
+  Lady Macbeth
+  Fat-tail
+  Miss Cleo 
+
 '''
